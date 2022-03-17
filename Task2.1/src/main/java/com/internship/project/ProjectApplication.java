ac85d97af5ac7dc15f7b235e3ba47cb3b0789f46
Ramadan Velisha <rvelisha@tu-sofia.bg>
2022-03-17 18:40:21 +0200

    Add files via upload

---
+++ Task2.1/src/main/java/com/internship/project/ProjectApplication.java
@@ -0,0 +1,66 @@
+package com.internship.project;
+
+import java.io.*;
+
+public class ProjectApplication {
+    public static void main(String[] args) throws IOException{
+
+        InputStream input = ProjectApplication.class.getResourceAsStream("/input.txt");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        String[] arrayOfElementsPerLine = new String[3];
+        String[][] arrayOfFileElements = new String[100][3];
+        int firstNumber;
+        int secondNumber;
+        String operation;
+        
+
+        try {
+
+            for (int i = 0;;i++) {
+
+                String currentLine = reader.readLine();
+
+                if (currentLine == null)
+                    break;
+
+                arrayOfElementsPerLine = currentLine.split(" ");
+
+                arrayOfFileElements[i][0] = arrayOfElementsPerLine[0];
+                arrayOfFileElements[i][1] = arrayOfElementsPerLine[1];
+
+                arrayOfFileElements[i][2] = arrayOfElementsPerLine[2];
+
+
+                firstNumber = Integer.parseInt(arrayOfFileElements[i][0]);
+                secondNumber = Integer.parseInt(arrayOfFileElements[i][1]);
+                operation = arrayOfFileElements[i][2];
+
+
+                switch (operation) {
+                    case "+":
+                        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+                        break;
+                    case "-":
+                        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+                        break;
+                    case "*":
+                        System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+                        break;
+                    case "/":
+                        System.out.println(String.format("%d / %d = %.2f", firstNumber, secondNumber, ((double) firstNumber / (double) secondNumber)));
+                        break;
+                    default:
+                        System.out.println("Choose a valid operation for this line!");
+                        break;
+                }
+
+            }
+
+        } catch (NumberFormatException exception) {
+            System.out.println("Enter a valid number on this line!");
+        }
+
+    }
+}
